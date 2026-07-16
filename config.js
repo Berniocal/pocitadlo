99d78164786a11b9c0a7d6d5970dfc3011ee108e
Bernio <jendabernard@gmail.com>
2026-07-16 20:25:18 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,17 +1,17 @@
 // Zkopírujte sem konfiguraci z Firebase Console:
 // Project settings → Your apps → Web app → SDK setup and configuration.
-export const firebaseConfig = {
-  apiKey: "DOPLNIT",
-  authDomain: "DOPLNIT.firebaseapp.com",
-  databaseURL: "https://DOPLNIT-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "DOPLNIT",
-  storageBucket: "DOPLNIT.firebasestorage.app",
-  messagingSenderId: "DOPLNIT",
-  appId: "DOPLNIT"
-};
+export   const firebaseConfig = {
+    apiKey: "AIzaSyDFAI8aooDX-v3NjexAbCQOwA-DVjQXAnA",
+    authDomain: "pocitadlo-32be7.firebaseapp.com",
+    databaseURL: "https://pocitadlo-32be7-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "pocitadlo-32be7",
+    storageBucket: "pocitadlo-32be7.firebasestorage.app",
+    messagingSenderId: "126699015808",
+    appId: "1:126699015808:web:b1e5b58dafdb97a60ae065"
+  };
 
 // Stejné adresy nastavte také v database.rules.json.
 export const allowedEmails = [
-  "prvni@gmail.com",
-  "druhy@gmail.com"
+  "jendabernard@gmail.com",
+  "klarabernardova@email.cz"
 ];
